bae295a38d73873888af3049c4d9418cc925caf0
suhang <suhang@xianhuahua.com>
2019-06-28 11:25:48 +0800

    又能运行

--- app/src/main/java/com/insightsurface/notebook/business/main/MainActivity.java
+++ app/src/main/java/com/insightsurface/notebook/business/main/MainActivity.java
@@ -1,9 +1,7 @@
 package com.insightsurface.notebook.business.main;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.insightsurface.lib.config.Configure;
 import com.insightsurface.notebook.R;
 import com.insightsurface.notebook.base.BaseActivity;
 
@@ -12,7 +10,6 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        baseToast.showToast(Configure.isTest(this)+"");
     }
 
     @Override
