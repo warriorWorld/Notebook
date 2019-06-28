9740f880deec53727be00c92044455fc224348ef
suhang <suhang@xianhuahua.com>
2019-06-28 18:26:56 +0800

    又能运行

--- app/src/main/java/com/insightsurface/notebook/business/main/MainActivity.java
+++ app/src/main/java/com/insightsurface/notebook/business/main/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
